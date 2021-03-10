988bfca704672731b9d5ec509ccec7a35be9ecc5
MrunalNagalwade <mrunalnagalwade187@gmail.com>
2021-03-10 18:44:56 +0530

    [mrunal]: Refactor operation assigning wages

--- Employeewage.java
+++ Employeewage.java
@@ -1,12 +1,19 @@
 public class Employeewage
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 {
 	int IS_FULL_TIME = 1;
+	int EMP_RATE_PER_HOUR = 20;
+	//VARIABLES
+	int empHrs = 0;
+	int empWage = 0;
 	double empCheck = Math.floor(Math.random() * 10) % 2;
 	if (empCheck == IS_FULL_TIME)
-		System.out.println("EMP IS PRESENT");
+		empHrs = 8;
 	else
-		System.out.println("EMP IS ABSENT");
+		empHrs = 0;
+	empWage = empHrs *  EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 }
 }
+
